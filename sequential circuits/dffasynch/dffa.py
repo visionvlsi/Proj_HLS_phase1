48978e018855d1849a8827ab1a2655752b1b95d2
visionvlsi <vvi.pvtltd@gmail.com>
2023-12-13 13:07:24 +0530

    dff with reset updated

--- sequential circuits/dffasynch/dffa.py
+++ sequential circuits/dffasynch/dffa.py
@@ -5,11 +5,18 @@ def dffa(q, d, clk, rst):
 
     @always_seq(clk.posedge, reset=rst)
     def logic():
-        '''
+        
         if rst == 0:    # nRst ?  or use active high ?
             q.next = 0
         else:
-            '''
-        q.next = d
+            q.next = d
 
     return logic
+
+def convert():
+    q, d, clk = [Signal(bool(0)) for i in range(3)]
+    rst = ResetSignal(val=1, active=0, isasync=True)
+    convInst = dffa(q, d, clk, rst)
+    convInst.convert(hdl='Verilog')
+
+convert()
