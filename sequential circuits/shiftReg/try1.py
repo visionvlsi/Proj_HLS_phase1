08340f49f7f16c662797f35615ee175a9e2aadff
visionvlsi <vvi.pvtltd@gmail.com>
2023-12-15 11:29:11 +0530

    shift register updated

--- sequential circuits/shiftReg/try1.py
+++ sequential circuits/shiftReg/try1.py
@@ -9,21 +9,19 @@ def ShiftRegister(clk, rst, data_in, shift_enable, data_out):
     @always_comb
     def logic():
         if shift_enable:
-            data_out.next = reg1[0]
-        else:
-            data_out.next = data_in
+            data_out.next = reg1[3]
 
-    @always_seq(clk.posedge, reset=rst)
+    @always_seq(clk.posedge,reset=rst)
     def shift_logic():
         if shift_enable:
-            reg1.next[1:] = reg1[0:3]  # Perform 1-bit left shift
+            reg1.next[4:1] = reg1[3:0]  # Perform 1-bit left shift
             reg1.next[0] = data_in    # Load data_in into the LSB position
 
     return logic, shift_logic
 
 def convert():
     clk ,data_in,data_out, shift_enable= [Signal(bool(0)) for i in range(4)]
-    rst = ResetSignal(val=0, active=0, isasync=True)
+    rst = ResetSignal(val=0, active=0, isasync=False)
     convInst = ShiftRegister(clk, rst, data_in, shift_enable, data_out)
     convInst.convert(hdl='Verilog')
 
